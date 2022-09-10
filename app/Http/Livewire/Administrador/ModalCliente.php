7d24096f6ebcf3458148a4f48e57f0266642b3bd
Catalina <catalina.poncep@utem.cl>
2022-09-10 01:34:12 -0300

    ADD: btn nuevo cliente

---
+++ app/Http/Livewire/Administrador/ModalCliente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Administrador;
+
+use Livewire\Component;
+
+class ModalCliente extends Component
+{
+    public function render()
+    {
+        return view('livewire.administrador.modal-cliente');
+    }
+}
--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -1,15 +1,20 @@
-
+<livewire:administrador.modal-cliente/>
+<link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 <div class="container-fluid"  style="height:100vh;background-color:#fdfdfd;">
-    <div class="row head">
+    <div class="row head d-flex justify-content-center">
         <div class="col-md-12 top">
             <img class="responsiveImg" src="http://5sconsultores.cl/portal/wp-content/uploads/2019/11/Logo_5s_web.jpg" alt="Logo"  class="brandlogo">
         </div>
         <div class="row titulo">Clientes</div>
+        <div class="row btn-addempresa" style="max-width:80rem;">
+                <button href="#" class="btn-post" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <i class="bi bi-plus"></i>Agregar nuevo cliente</button>
+        </div>
     </div>
     <div class="d-flex justify-content-center section">
-        <div class="row row-cols-auto group-cards overflow-auto" style="max-width:90rem;max-height:70vh">
-            @for ($i = 0; $i < 8; $i++)
-            <div class="col-md-12" style="width:14rem">
+        <div class="row row-cols-auto group-cards overflow-auto" style="max-width:75rem;max-height:70vh">
+            @for ($i = 0; $i < 7; $i++)
+            <div class="col-md-12" style="width:18rem">
                 <div class="card my-2">
                     <div class="card-body">
                       <h5 class="card-title">Nombre empresa nombre empresa</h5>
