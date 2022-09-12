a34225c2b76781f1836268e6372113a35139c523
Fernando DC <fernando.dc.dex@gmail.com>
2022-09-11 21:53:03 -0300

    FIX: Livewire - new components

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'admin' => 'App\\Http\\Livewire\\Admin',
+  'administrador.contactos' => 'App\\Http\\Livewire\\Administrador\\Contactos',
+  'cliente' => 'App\\Http\\Livewire\\Cliente',
+  'components-cliente.navbar' => 'App\\Http\\Livewire\\ComponentsCliente\\Navbar',
+  'login' => 'App\\Http\\Livewire\\Login',
+  'perfiles' => 'App\\Http\\Livewire\\Perfiles',
+);
