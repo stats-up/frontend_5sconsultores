ea42dd5f08a70cf8ce7ffef99c3c3e878e2aacb0
Catalina <catalina.poncep@utem.cl>
2022-10-01 17:14:42 -0300

    ADD: listar requerimientos cliente

--- app/Http/Livewire/Cliente.php
+++ app/Http/Livewire/Cliente.php
@@ -3,9 +3,27 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Log;
 
 class Cliente extends Component
-{
+{   
+    public $requerimientos = [];
+
+    public function get_all_requerimientos(){
+        $token = getenv("API_TOKEN");
+        $array["token"] = $token;
+        $array["data"]["id_user"] = Session::get('user')["id"];
+        $endpoint = getenv("API_URL")."/api/get_requests_by_id_user";
+        $response = Http::withBody(json_encode($array), 'application/json')->post($endpoint);
+        $this->requerimientos = $response->json();
+    }
+
+    public function mount(){
+        $this->get_all_requerimientos();
+    }
+
     public function render()
     {
         return view('livewire.cliente');
--- resources/views/livewire/cliente.blade.php
+++ resources/views/livewire/cliente.blade.php
@@ -28,31 +28,23 @@
     </div>
     <div class="row overflow-auto list" style="max-height:70vh;justify-content:center;">
         <div class="col" style="max-width:65rem;">
-            @for ($i = 0; $i < 13; $i++)
+            @foreach ($requerimientos as $req)
             <div class="card row justify-content-center target">
                 <div class="card-body row mx-0">
                     <div class="col-lg-9" style="height:fit-content">
-                        <div class="cargo" >Ingeniero Senior de Metalurgia</div>
+                        <div class="cargo" >{{$req["nombre"]}}</div>
                         <div class="descripcion py-2">
-                            <div> Profesión: Ingeniero Civil/ Ejecución Quimica/ Metalurgico</div>
-                            <div> Experiencia: Al menos 8 años de en procesos de plantas</div>
-                            <div> Experiencia: Al menos 4 años en plantas de tratamientos minerales sulfurados de cobre</div>
-                            <div> Experiencia: En Metalurgia Extractiva de cobre por via concentradora</div>
-                            <div> Manejo de Herramientas: Microsoft Office a nivel usuario</div>
-                        </div>
-                        <div class="condiciones py-2">
-                            <div> Turno: 4x3</div>
-                            <div> Faena:Mantos Blancos</div>
+                            {{$req["descripcion"]}}
                         </div>
                     </div>
                     <div class="col-lg-3 py-2 text-center">
                         <button class="btn-postulantes" style="width:80%;">
-                            <a href="/perfiles" class="btn-post">Ver postulantes (8)</a>
+                            <a href="/perfiles" class="btn-post">Ver postulantes</a>
                         </button>
                     </div>
                 </div>
             </div>   
-            @endfor
+            @endforeach
         </div>
     </div>
     <script>
