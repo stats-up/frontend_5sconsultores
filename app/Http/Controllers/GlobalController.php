6bc1554e0ba12552bf1505d780edd1a8cc07b902
Fernando DC <fernando.dc.dex@gmail.com>
2022-10-01 17:32:12 -0300

    ADD: Estructura mail

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MailStructure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Mail;
 
 class GlobalController extends Controller
 {
     public function logout(){
         Session::flush();
         return redirect('/');
+    }
+    public function sendEmail(Request $request){
+        if(Session::has('user')){
+            $to = "";
+            Mail::to($to)->queue(new MailStructure($title,$message));
+        }
     }
 }
---
+++ app/Mail/MailStructure.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MailStructure extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject = "";
+    public $msg = "";
+
+    public function __construct($titulo,$mensaje)
+    {
+        $this->subject = $titulo;
+        $this->msg = $mensaje;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
